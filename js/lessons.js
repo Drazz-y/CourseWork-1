c527aafb3138642830030fb2cf4af7ec85253aca
Andrew Okwudili <andrewokwudili599@gmail.com>
2023-11-21 02:20:17 +0400

    dataset

---
+++ js/lessons.js
@@ -0,0 +1,67 @@
+let lessons = [
+    {
+      "id": 1001,
+      "subject": "Swimming",
+      "location": "London",
+      "price": 200,
+      "spaces": 5,
+      "cart_quantity": 0
+    },
+     {
+       "id": 1002,
+       "subject": "Golf",
+       "location": "Dubai",
+       "price": 1000,
+       "spaces": 5,
+       "cart_quantity": 0
+     },
+     {
+       "id": 1003,
+       "subject": "Gaming",
+       "location": "Madrid",
+       "price": 300,
+       "spaces": 5,
+       "cart_quantity": 0
+     },
+     {
+       "id": 1004,
+       "subject": "Archery",
+       "location": "Barcelona",
+       "price": 400,
+       "spaces": 5,
+       "cart_quantity": 0
+     },
+     {
+     "id": 1005,
+     "subject": "Maths",
+     "location": "Helsinki",
+     "price": 250,
+     "spaces": 5,
+     "cart_quantity": 0
+   },
+     {
+       "id": 1006,
+       "subject": "Reading",
+       "location": "Valletta",
+       "price": 100,
+       "spaces": 5,
+       "cart_quantity": 0
+     },
+     {
+       "id": 1007,
+       "subject": "Painitng",
+       "location": "Nairobi",
+       "price": 800,
+       "spaces": 5,
+       "cart_quantity": 0
+     },
+     {
+       "id": 1008,
+       "subject": "Economics",
+       "location": "India",
+       "price": 700,
+       "spaces": 5,
+       "cart_quantity": 0
+     }
+  ]
+  
